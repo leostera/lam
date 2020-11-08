2b28df48aef101b3bce00a6d65491f3ed8ebb6c6
Leandro Ostera <leandro@ostera.io>
2020-11-09 00:02:25 +0100

    lam-bin: bundle debug/release liblamrts appropriately

--- lib/lam-bin/src/bingen.rs
+++ lib/lam-bin/src/bingen.rs
@@ -1,6 +1,13 @@
-use anyhow::{Context, Error};
-use log::{debug, info};
+use anyhow::{anyhow, Context, Error};
+use log::debug;
+use std::io::Write;
 use std::path::PathBuf;
+
+#[cfg(debug_assertions)]
+const RUNTIME: &[u8] = include_bytes!("../../../target/debug/liblamrts.a");
+
+#[cfg(not(debug_assertions))]
+const RUNTIME: &[u8] = include_bytes!("../../../target/release/liblamrts.a");
 
 #[derive(Clone, Debug, Default)]
 pub struct Target {
@@ -30,15 +37,17 @@ impl Target {
             std::slice::from_raw_parts(bc_ptr, bc_size)
         };
 
-        let template = include_str!("bin.c").to_string();
+        let runtime_object = std::path::PathBuf::from(&"./liblamrts.a");
+        let mut liblamrts_a = std::fs::File::create(&runtime_object)?;
+        liblamrts_a.write_all(RUNTIME)?;
 
+        let template = include_str!("bin.c").to_string();
         let bc_str = format!("{:?}", data);
         let c_code = template
             .replace("LAM_BYTECODE_SIZE", &bc_size.to_string())
             .replace("LAM_BYTECODE_RAW", &bc_str)
             .replace("{[", "{")
             .replace("]}", "}");
-
         let path = std::path::PathBuf::from(&"./native.c");
         std::fs::write(&path, c_code).context("Could not write .c file")?;
 
@@ -48,8 +57,8 @@ impl Target {
         cc.args(&[
             "-o",
             &self.output.to_str().context("Bad output path")?,
-            "native.c",
-            "./target/debug/liblamrts.a",
+            &path.to_str().context("Bad .c file path")?,
+            &runtime_object.to_str().context("Bad runtime object path")?,
             "-lpthread",
             "-ldl",
             "-lm",
@@ -58,6 +67,7 @@ impl Target {
 
         debug!("Removing temporary files...");
 
+        std::fs::remove_file(runtime_object).context("Could not remove liblamrts.a file")?;
         std::fs::remove_file(path).context("Could not remove .c file")?;
 
         Ok(res)
