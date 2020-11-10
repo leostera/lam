4b686b213f8fa90bc51af330e9eae6b87ebdd7fc
Leandro Ostera <leandro@ostera.io>
2020-11-10 20:25:38 +0100

    examples: setup build script and web boilerplate

---
+++ examples/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+caramelc compile hello_joe.ml
+
+erlc -S hello_joe.erl
+erlc hello_joe.erl
+
+lam build hello_joe.beam -o hello_joe.exe -t native
+lam build hello_joe.beam -o hello_joe.wasm -t wasm
+
+mkdir -p web
+lam build hello_joe.beam -o web/hello_joe.wasm -t web
+
