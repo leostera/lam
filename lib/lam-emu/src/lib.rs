31fc4dc60c3a53790efed29554e58f0e11d18d89
Leandro Ostera <leandro@ostera.io>
2020-11-11 14:25:22 +0100

    emu: move byteops to beam crate

--- lib/lam-emu/src/lib.rs
+++ lib/lam-emu/src/lib.rs
@@ -1,10 +1,8 @@
-pub mod byteops;
 pub mod emulator;
 pub mod program;
 pub mod runner;
 pub mod runtime;
 
-pub use self::byteops::*;
 pub use self::emulator::*;
 pub use self::program::*;
 pub use self::runner::*;
