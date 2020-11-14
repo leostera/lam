04f1af38afd37bfedfd049a0db28bf87e94df444
Leandro Ostera <leandro@ostera.io>
2020-11-14 21:26:49 +0100

    comp: first working translator with a few opcodes!

--- lib/lam-compiler/src/translator.rs
+++ lib/lam-compiler/src/translator.rs
@@ -1,105 +1,227 @@
-use lam_beam::{AtomTable, Chunk, CodeTable, LiteralTable, OpCode, BEAM};
-use lam_emu::{Instruction, Program, MFA};
+use lam_beam::external_term;
+use lam_beam::{
+    AtomTable, Chunk, CodeTable, CompactTerm, ExportTable, ExternalTerm, ImportTable, LiteralTable,
+    OpCode, BEAM,
+};
+use lam_emu::{
+    FnCall, FunctionLabel, Instruction, List, Literal, Module, Program, Register, Value,
+};
 
 #[derive(Debug, Clone, Default)]
-pub struct Translator {
-    bytecode: Vec<BEAM>,
+#[repr(C)]
+pub struct ModuleTranslator {
+    beam_export_table: ExportTable,
+    beam_import_table: ImportTable,
+    beam_atom_table: AtomTable,
+    beam_code_table: CodeTable,
+    beam_literal_table: LiteralTable,
+    lam_module: Module,
 }
 
-impl Translator {
-    pub fn with_bytecode(self, bytecode: Vec<BEAM>) -> Translator {
-        Translator { bytecode, ..self }
-    }
-
-    pub fn to_program(self) -> Program {
-        let instructions = self
-            .bytecode
-            .iter()
-            .flat_map(|beam| Translator::beam_to_instructions(beam))
-            .collect();
-
-        Program::from_instructions(instructions)
-    }
-
-    pub fn beam_to_instructions(beam: &BEAM) -> Vec<Instruction> {
-        let mut instrs = vec![];
-
-        let mut atom_table = AtomTable::default();
-        let mut code_table = CodeTable::default();
-        let mut literal_table = LiteralTable::default();
-
+/// The translator module takes a number of BEAM bytecode structures and turns
+/// them into a Program runnable by the emulator.
+///
+/// To do this, it needs to traverse all the instructions of the BEAM bytecodes,
+/// and build up the appropriate modules, and functions, as well as pointing
+/// to the _main_ function that the program will execute first.
+///
+impl ModuleTranslator {
+    pub fn from_bytecode(&mut self, beam: BEAM) -> &mut ModuleTranslator {
         for chunk in beam.chunks() {
             match chunk {
-                Chunk::AtU8(chunk_data) => atom_table = chunk_data.data.clone(),
-                Chunk::Atom(chunk_data) => atom_table = chunk_data.data.clone(),
-                Chunk::Code(chunk_data) => code_table = chunk_data.data.clone(),
-                Chunk::LitT(chunk_data) => literal_table = chunk_data.data.clone(),
+                Chunk::AtU8(chunk_data) => self.beam_atom_table = chunk_data.data.clone(),
+                Chunk::Atom(chunk_data) => self.beam_atom_table = chunk_data.data.clone(),
+                Chunk::Code(chunk_data) => self.beam_code_table = chunk_data.data.clone(),
+                Chunk::LitT(chunk_data) => self.beam_literal_table = chunk_data.data.clone(),
+                Chunk::ExpT(chunk_data) => self.beam_export_table = chunk_data.data.clone(),
+                Chunk::ImpT(chunk_data) => self.beam_import_table = chunk_data.data.clone(),
+                _ => (),
+            }
+        }
+        self.lam_module = self.build_module();
+        self
+    }
+
+    pub fn module(&self) -> Module {
+        self.lam_module.clone()
+    }
+
+    fn build_module(&self) -> Module {
+        let mut module = Module::default();
+        module.name = self.beam_atom_table.atoms[0].name.clone();
+
+        for _ in 1..=(self.beam_code_table.label_count as u8) {
+            module.labels.push(FunctionLabel::default());
+        }
+
+        let mut current_label: u8 = 0;
+
+        /* NOTE(@ostera): the atom table really begins at 1 instead of 0, but
+         * when we read it, we index it from 0, so all atom indices are off by 1 */
+        for export in self.beam_export_table.exports.iter() {
+            let atom_idx = export.atom_index - 1;
+
+            let fn_name = &self.beam_atom_table.atoms[atom_idx as usize].name;
+            let fn_arity = export.arity as u8;
+            let fn_first_label = export.label as u8;
+
+            let key = (fn_name.to_string(), fn_arity);
+            module.functions.insert(key, fn_first_label);
+        }
+
+        /* We'll go over every instruction, batching them by the current label id */
+        for (opcode, _arity, args) in self.beam_code_table.instructions() {
+            println!("\n{:?} {:?}", opcode, args);
+            match opcode {
+                /* This OpCode defines the _beginning_ of a function section.
+                 *
+                 * If there is none, then we won't actually do anything in this
+                 * function and you'll end up with an empty module. */
+                OpCode::Label => {
+                    current_label = args[0].clone().into();
+                }
+
+                /* If we are in a label, try to translate the instructions into
+                 * something we can handle in the emulator. */
+                _ if current_label > 0 => {
+                    if let Some(lam_instr) = ModuleTranslator::mk_instr(
+                        &opcode,
+                        &args,
+                        &self.beam_atom_table,
+                        &self.beam_literal_table,
+                        &self.beam_import_table,
+                    ) {
+                        println!("=> {:?}", lam_instr);
+                        module.labels[(current_label - 1) as usize]
+                            .instructions
+                            .push(lam_instr);
+                    }
+                }
                 _ => (),
             }
         }
 
-        for (opcode, _arity, args) in code_table.instructions() {
-            if let Some(instr) =
-                Translator::mk_instr(opcode, args, &atom_table, &code_table, &literal_table)
-            {
-                instrs.push(instr)
-            }
-        }
-
-        instrs
+        module
     }
 
-    fn mk_instr(
+    pub fn mk_instr(
         opcode: &lam_beam::OpCode,
         args: &std::vec::Vec<lam_beam::CompactTerm>,
         atom_table: &lam_beam::AtomTable,
-        code_table: &lam_beam::CodeTable,
         literal_table: &lam_beam::LiteralTable,
+        import_table: &lam_beam::ImportTable,
     ) -> Option<Instruction> {
         /* using the args, look up the right values in the right tables */
         match opcode {
-            OpCode::Allocate => Some(Instruction::Allocate(args[0].into(), args[1].into())),
-            OpCode::CallExtOnly => Some(Instruction::CallExtOnly {
-                arity: args[0].into(),
-                mfa: {
-                    let (module, function, arity) = args[1].into();
-                    let module = atom_table.atoms[module as usize].name;
-                    let function = atom_table.atoms[function as usize].name;
-                    MFA {
-                        module,
-                        function,
-                        arity,
-                    }
-                },
-            }),
-            OpCode::CallOnly => Some(Instruction::CallOnly {
-                arity: args[0].into(),
-                label: args[1].into(),
-            }),
-            OpCode::Label => Some(Instruction::Label { id: args[0].into() }),
             OpCode::Move => {
-                let from = Translator::mk_value(args[0]);
-                let to = Translator::mk_reg(args[1]);
+                let from = ModuleTranslator::mk_value_of_compact_term(
+                    args[0].clone(),
+                    &atom_table,
+                    &literal_table,
+                );
+                let to = ModuleTranslator::mk_reg(args[1].clone());
                 Some(Instruction::Move(from, to))
+            }
+            OpCode::CallExtOnly => {
+                let import_idx: u8 = args[1].clone().into();
+                let import = &import_table.imports[import_idx as usize];
+
+                let module = atom_table.atoms[(import.module_atom_index - 1) as usize]
+                    .name
+                    .to_string();
+                let function = atom_table.atoms[(import.fun_atom_index - 1) as usize]
+                    .name
+                    .to_string();
+                let arity = import.arity as u8;
+
+                Some(Instruction::Call(FnCall::Qualified {
+                    module,
+                    function,
+                    arity,
+                }))
             }
             _ => None,
         }
     }
 
-    fn mk_value(x: CompactTerm) -> Value {
+    pub fn mk_value_of_compact_term(
+        x: CompactTerm,
+        atom_table: &lam_beam::AtomTable,
+        literal_table: &lam_beam::LiteralTable,
+    ) -> Value {
         match x {
-            RegisterX(x) => Value::Register(Register::X(x)),
-            RegisterY(x) => Value::Register(Register::Y(y)),
-            Nil => Value::Nil,
-            Literal(idx) => (),
-            Integer(Value) => (),
-            Atom(idx) => (),
-            Label(idx) => (),
-            Character(u8) => (),
-            List => (),
-            RegisterFloat => (),
-            ListAllocation => (),
-            ExtendedLiteral(Value) => (),
+            CompactTerm::RegisterX(x) => Value::Register(Register::X(x)),
+            CompactTerm::RegisterY(y) => Value::Register(Register::Y(y)),
+            CompactTerm::Nil => Value::Literal(Literal::List(List::Nil)),
+            CompactTerm::Integer(v) => ModuleTranslator::mk_int(v),
+            CompactTerm::Character(c) => Value::Literal(Literal::Character(c)),
+            CompactTerm::Atom(idx) => Value::Literal(Literal::Atom(
+                atom_table.atoms[(idx - 1) as usize].name.to_string(),
+            )),
+            CompactTerm::Literal(idx)
+            | CompactTerm::ExtendedLiteral(lam_beam::Value::Small(idx)) => {
+                Value::Literal(ModuleTranslator::mk_literal_of_external_term(
+                    &literal_table.literals[idx as usize],
+                ))
+            }
+            CompactTerm::ExtendedLiteral(v) => ModuleTranslator::mk_int(v),
+            _ => panic!(
+                "Don't know how to turn CompactTerm {:?} into a lam_emu::Value",
+                x
+            ),
         }
+    }
+
+    pub fn mk_literal_of_external_term(x: &ExternalTerm) -> Literal {
+        match x {
+            ExternalTerm::List(external_term::List { elements }) => {
+                let value = elements.iter().fold(List::Nil, |acc, el| {
+                    List::Cons(
+                        Box::new(ModuleTranslator::mk_literal_of_external_term(el)),
+                        Box::new(acc),
+                    )
+                });
+                Literal::List(value)
+            }
+            ExternalTerm::Atom(atom) => Literal::Atom(atom.to_string()),
+            ExternalTerm::Binary(bin) => Literal::Binary(
+                String::from_utf8(bin.bytes.clone())
+                    .expect("Binary string had invalid utf-8 characters"),
+            ),
+            _ => panic!(
+                "Don't know how to turn ExternalTerm {:?} into a lam_emu::Value",
+                x
+            ),
+        }
+    }
+
+    pub fn mk_int(x: lam_beam::Value) -> Value {
+        match x {
+            lam_beam::Value::Small(y) => Value::Literal(Literal::Integer(y.into())),
+            lam_beam::Value::Large(z) => Value::Literal(Literal::Integer(z)),
+        }
+    }
+
+    pub fn mk_reg(x: CompactTerm) -> Register {
+        match x {
+            CompactTerm::RegisterX(x) => Register::X(x),
+            CompactTerm::RegisterY(y) => Register::Y(y),
+            _ => panic!("Tried to turn {:?} into a register", x),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+#[repr(C)]
+pub struct Translator {}
+
+impl Translator {
+    pub fn from_bytecode(&mut self, beams: Vec<BEAM>) -> lam_emu::Program {
+        let mut modules = vec![];
+        for beam in beams {
+            let module = ModuleTranslator::default().from_bytecode(beam).module();
+            modules.push(module);
+        }
+        Program::default().with_modules(modules)
     }
 }
