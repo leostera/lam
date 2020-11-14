0f43c402f838aea00c27638a2e862f33d5608b13
Leandro Ostera <leandro@ostera.io>
2020-11-14 23:18:47 +0100

    rts(web): implement web runtime to new trait

--- lib/lam-rts-web/src/lib.rs
+++ lib/lam-rts-web/src/lib.rs
@@ -7,5 +7,5 @@ pub fn start(data: *const u8, size: usize) {
     let data: &[u8] = unsafe { std::slice::from_raw_parts(data, size) };
     let program = lam_emu::program::Program::deserialize(&data).unwrap();
     let runtime = web_runtime::WebRuntime::default();
-    lam_emu::Runner::for_program(program, Box::new(runtime)).run()
+    lam_emu::Emulator::new(program, Box::new(runtime)).run()
 }
--- lib/lam-rts-web/src/web_runtime.rs
+++ lib/lam-rts-web/src/web_runtime.rs
@@ -1,30 +1,23 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-use lam_emu::{Emulator, Runtime, MFA};
+use lam_emu::{Literal, Runtime, Value, MFA};
 
 #[wasm_bindgen]
 #[derive(Default, Debug, Clone)]
 pub struct WebRuntime {}
 
 impl Runtime for WebRuntime {
-    fn execute(&mut self, mfa: &MFA, emu: &mut Emulator) {
+    fn execute(&mut self, mfa: &MFA, args: &[Value]) -> Value {
         let MFA {
             module,
             function,
             arity,
         } = mfa;
         match (module.as_str(), function.as_str()) {
-            ("io", "format") => {
-                let mut regs = vec![];
-                for i in 0..*arity {
-                    regs.push(emu.registers[i as usize].clone());
-                }
-                match regs[1] {
-                    _ => console::log_1(&format!("{:?}", regs[0]).into()),
-                }
-            }
-            (_, _) => todo!(),
-        }
+            ("io", "format") => console::log_1(&format!("{:?}", args).into()),
+            (_, _) => panic!("How'd you get here?"),
+        };
+        Value::Literal(Literal::Atom("ok".to_string()))
     }
 }
