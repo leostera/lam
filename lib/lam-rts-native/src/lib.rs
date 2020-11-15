0507c3b2721c7daf97c4d6f9b1a1eb125676af3b
Leandro Ostera <leandro@ostera.io>
2020-11-15 13:22:12 +0100

    rts(native): implement - and +

--- lib/lam-rts-native/src/lib.rs
+++ lib/lam-rts-native/src/lib.rs
@@ -1,3 +1,5 @@
+use log::debug;
+
 mod native_runtime;
 
 /** # Safety
@@ -6,7 +8,12 @@ mod native_runtime;
 #[export_name = "lam_rts__start"]
 pub unsafe extern "C" fn start(data: *const u8, size: usize) {
     let data: &[u8] = std::slice::from_raw_parts(data, size);
-    let program = lam_emu::program::Program::deserialize(&data).unwrap();
+    let program = lam_emu::program::Program::deserialize(&data)
+        .expect("could not decode program! has this been serialized correctly?");
     let runtime = native_runtime::NativeRuntime::default();
+
+    env_logger::init();
+    debug!("Initializing Native Runtime...");
+
     lam_emu::Emulator::new(program, Box::new(runtime)).run()
 }
--- lib/lam-rts-native/src/native_runtime.rs
+++ lib/lam-rts-native/src/native_runtime.rs
@@ -1,19 +1,38 @@
-use lam_emu::{Literal, Runtime, Value, MFA};
+use lam_emu::{List, Literal, Runtime, Value, MFA};
+use num_bigint::BigInt;
 
 #[derive(Default, Debug, Clone)]
 pub struct NativeRuntime {}
 
 impl Runtime for NativeRuntime {
-    fn execute(&mut self, mfa: &MFA, args: &[Value]) -> Value {
+    fn execute(&mut self, mfa: &MFA, args: &[Literal]) -> Literal {
         let MFA {
             module,
             function,
             arity: _,
         } = mfa;
         match (module.as_str(), function.as_str()) {
-            ("io", "format") => println!("{:?}", args),
+            ("io", "format") => {
+                match args[1].clone() {
+                    Literal::List(List::Cons(boxed_int, _)) => match *boxed_int {
+                        Value::Literal(Literal::Integer(bi)) => println!("{}", bi.to_string()),
+                        x => println!("{:?}", x),
+                    },
+                    x => println!("{:?}", x),
+                }
+                Literal::Atom("ok".to_string())
+            }
+            ("erlang", "-") => {
+                let a: BigInt = args[0].clone().into();
+                let b: BigInt = args[1].clone().into();
+                Literal::Integer(a - b)
+            }
+            ("erlang", "+") => {
+                let a: BigInt = args[0].clone().into();
+                let b: BigInt = args[1].clone().into();
+                Literal::Integer(a + &b)
+            }
             (_, _) => panic!("How'd you get here?"),
-        };
-        Value::Literal(Literal::Atom("ok".to_string()))
+        }
     }
 }
