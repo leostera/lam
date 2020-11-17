08fbd1fa020b33b495e709f787a3b0178276ccf2
Leandro Ostera <leandro@ostera.io>
2020-11-17 01:02:24 +0100

    emu: extend runtime trait to include scheduler execution, sleeping, and halting

--- lib/lam-emu/src/runtime.rs
+++ lib/lam-emu/src/runtime.rs
@@ -1,9 +1,17 @@
 use super::bytecode::*;
 use super::program::*;
+use super::scheduler::*;
+use anyhow::Error;
 
 /** This trait represents a runtime system and is used to plug the emulator's
  *  side-effects and other functions implemented by the host.
  */
 pub trait Runtime {
     fn execute(&mut self, call: &MFA, args: &[Literal]) -> Literal;
+
+    fn run_schedulers(&mut self, schedulers: Vec<Scheduler>) -> Result<(), Error>;
+
+    fn sleep(&self, duration: u64);
+
+    fn halt(&self) -> !;
 }
