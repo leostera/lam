ccc6385d089b3e42345242e6965dad341b7631a1
Leandro Ostera <leandro@ostera.io>
2020-11-20 18:01:58 +0100

    emu: properly save continuation pointer when calling/jumping

--- lib/lam-emu/src/instr_ptr.rs
+++ lib/lam-emu/src/instr_ptr.rs
@@ -1,4 +1,5 @@
 use super::bytecode::*;
+use super::literal::*;
 use super::program::*;
 use std::boxed::Box;
 
@@ -34,46 +35,57 @@ impl InstructionPointer {
         }
     }
 
-    pub fn next(&mut self, program: &Program) {
+    pub fn get_next(&self, program: &Program) -> InstructionPointer {
         let module = program.modules.get(&self.current_module).unwrap();
 
         let label = (self.current_label) as usize;
         let instructions = &module.labels[label].instructions;
 
-        // let last_label = module.labels.len();
         let last_offset = instructions.len();
 
         let next_instr = self.current_instruction + 1;
+
+        let mut next_instr_ptr = self.clone();
         if next_instr < last_offset {
-            self.current_instruction += 1;
-            self.instr = instructions[self.current_instruction as usize].clone()
+            next_instr_ptr.current_instruction += 1;
+            next_instr_ptr.instr =
+                instructions[next_instr_ptr.current_instruction as usize].clone();
         } else if let Some(last_ptr) = &self.last_instr_ptr {
-            *self = (**last_ptr).clone()
+            next_instr_ptr = (**last_ptr).clone();
         } else {
-            self.instr = Instruction::Halt
+            next_instr_ptr.instr = Instruction::Halt;
         }
+        next_instr_ptr
     }
 
-    pub fn jump(&mut self, program: &Program, call: &FnCall) {
-        let last_ptr = self.clone();
+    pub fn next(&mut self, program: &Program) {
+        *self = self.get_next(&program);
+    }
+
+    pub fn call(&mut self, program: &Program, call: &FnCall) {
+        let next_ptr = self.get_next(&program);
 
         let module_name = call
             .module()
-            .unwrap_or_else(|| last_ptr.current_module.clone());
+            .unwrap_or_else(|| next_ptr.current_module.clone());
         let module = program
             .modules
             .get(&module_name)
             .unwrap_or_else(|| panic!("Could not find module: {:?}", &module_name));
-        /* NOTE: labels are 1 indexed! */
-        let function_key = (call.function(), call.arity());
-        let first_label = module
-            .functions
-            .get(&function_key)
-            .unwrap_or_else(|| panic!("Could not find function : {:?}", &function_key));
+        let first_label = match call {
+            FnCall::Local { label, .. } => label,
+            _ => {
+                let function_key = (call.function(), call.arity());
+                module
+                    .functions
+                    .get(&function_key)
+                    .unwrap_or_else(|| panic!("Could not find function : {:?}", &function_key))
+            }
+        };
         let first_instruction = module.labels[*first_label as usize].instructions[0].clone();
 
         *self = InstructionPointer {
-            last_instr_ptr: Some(Box::new(last_ptr)),
+            last_instr_ptr: Some(Box::new(next_ptr)),
             current_module: module.name.clone(),
             current_label: *first_label,
             current_instruction: 0,
@@ -84,9 +96,9 @@ impl InstructionPointer {
     pub fn jump_to_label(&mut self, program: &Program, label: &Label) {
         trace!("Jumping to label: {:?}", label);
 
-        let last_ptr = self.clone();
+        let next_ptr = self.get_next(&program);
 
-        let module_name = last_ptr.current_module.clone();
+        let module_name = self.current_module.clone();
         let module = program
             .modules
             .get(&module_name)
@@ -97,11 +109,11 @@ impl InstructionPointer {
         trace!("First instruction: {:?}", first_instruction);
 
         *self = InstructionPointer {
-            current_module: last_ptr.current_module,
+            current_module: module_name,
             current_label: *label,
             current_instruction: 0,
             instr: first_instruction,
-            last_instr_ptr: None,
+            last_instr_ptr: Some(Box::new(next_ptr)),
         }
     }
 
