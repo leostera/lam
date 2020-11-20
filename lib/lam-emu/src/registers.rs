8e06f47b2ec495e1f8381979f044697c753ac730
Leandro Ostera <leandro@ostera.io>
2020-11-20 18:03:55 +0100

    emu: add register helpers for implementing lambdas

--- lib/lam-emu/src/registers.rs
+++ lib/lam-emu/src/registers.rs
@@ -1,6 +1,7 @@
 use super::bytecode::*;
 use super::literal::*;
 use std::collections::VecDeque;
+use std::fmt::{Display, Formatter};
 
 use log::*;
 
@@ -14,7 +15,7 @@ pub struct Registers {
 
 impl Registers {
     pub fn new() -> Registers {
-        let empty: Vec<Value> = [0; 32].to_vec().iter().map(|_| Value::Nil).collect();
+        let empty: Vec<Value> = [0; 4].to_vec().iter().map(|_| Value::Nil).collect();
         Registers {
             global: empty.clone(),
             local: VecDeque::new(),
@@ -22,8 +23,8 @@ impl Registers {
         }
     }
 
-    pub fn clear(&mut self, r: Register) -> &mut Registers {
-        self.put(r, Value::Nil);
+    pub fn clear(&mut self, r: &Register) -> &mut Registers {
+        self.put(&r, Value::Nil);
         self
     }
 
@@ -34,21 +35,28 @@ impl Registers {
         self
     }
 
+    pub fn fill_globals(&mut self, vs: &Vec<Literal>) -> &mut Registers {
+        for (i, v) in vs.iter().enumerate() {
+            self.global[i as usize] = v.clone().into();
+        }
+        self
+    }
+
     pub fn put_global(&mut self, n: u32, v: Value) -> &mut Registers {
         self.global[n as usize] = v.clone();
         self
     }
 
-    pub fn put(&mut self, r: Register, v: Value) -> &mut Registers {
+    pub fn put(&mut self, r: &Register, v: Value) -> &mut Registers {
         match r {
-            Register::Local(l) => self.current_local[l as usize] = v.clone(),
-            Register::Global(g) => self.global[g as usize] = v.clone(),
+            Register::Local(l) => self.current_local[*l as usize] = v.clone(),
+            Register::Global(g) => self.global[*g as usize] = v.clone(),
         }
         self
     }
 
     pub fn get(&self, r: &Register) -> Value {
-        trace!("fetching register: {:?}", r);
+        trace!("fetching register: {}", r);
         match r {
             Register::Local(l) => self.current_local[*l as usize].clone(),
             Register::Global(g) => self.global[*g as usize].clone(),
@@ -56,6 +64,7 @@ impl Registers {
     }
 
     pub fn get_many_literals_from_global_range(&self, from: u32, to: u32) -> Vec<Literal> {
+        trace!("fetching global values from {} to {}", from, to);
         self.global[from as usize..to as usize]
             .iter()
             .map(|v| self.get_literal_from_value(v))
@@ -63,12 +72,18 @@ impl Registers {
     }
 
     pub fn get_literal_from_value(&self, v: &Value) -> Literal {
-        trace!("fetching value: {:?}", v);
+        trace!("fetching value: {}", v);
         match v {
             Value::Literal(l) => l.clone(),
             Value::Register(reg) => self.get_literal_from_value(&self.get(reg)),
             Value::Nil => Literal::List(List::Nil),
         }
+    }
+
+    pub fn copy(&mut self, a: &Register, b: &Register) -> &mut Registers {
+        trace!("copy value from {} to {}", a, b);
+        self.put(b, self.get(&a));
+        self
     }
 
     pub fn swap(&mut self, a: Register, b: Register) {
@@ -86,5 +101,19 @@ impl Registers {
                 self.global[g as usize] = temp;
             }
         }
+    }
+}
+
+impl Display for Registers {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(fmt, "Globals {{")?;
+        for i in 0..self.global.len() {
+            write!(fmt, "{}: {}, ", i, &self.global[i])?
+        }
+        write!(fmt, "}}; Locals {{")?;
+        for i in 0..self.current_local.len() {
+            write!(fmt, "{}: {}, ", i, &self.current_local[i])?
+        }
+        write!(fmt, "}}")
     }
 }
