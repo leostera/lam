399d46d976c3a1b770317eb2f158c51af8241469
Leandro Ostera <leandro@ostera.io>
2020-11-20 18:04:33 +0100

    emu: implement MakeLambda and ApplyLambda
    * fix Call instructions
    * fix improper lists trying to stay proper (now they can't be improper)
    * various register fixes
    * explicitly ignore some instructions for now

--- lib/lam-emu/src/emulator.rs
+++ lib/lam-emu/src/emulator.rs
@@ -35,7 +35,7 @@ impl Emulator {
     }
 
     pub fn preload(&mut self, x: u32, v: Value) -> &mut Emulator {
-        self.registers.put(Register::Global(x), v);
+        self.registers.put(&Register::Global(x), v);
         self
     }
 
@@ -45,11 +45,12 @@ impl Emulator {
         program: &Program,
         _scheduler: &mut Scheduler,
         runtime: &mut Box<dyn Runtime>,
+        pid: Pid,
     ) -> Result<EmulationStatus, Error> {
         let mut reductions = 0;
         while reductions < reduction_count {
-            trace!("Reductions: {:?}", reductions);
-            trace!("Registers: {:?}", self.registers,);
+            trace!("Reductions: {}", reductions);
+            trace!("Registers: {}", self.registers,);
             trace!("Instr => {:?}", self.instr_ptr.instr);
             match self.instr_ptr.instr.clone() {
                 ////////////////////////////////////////////////////////////////
@@ -63,7 +64,14 @@ impl Emulator {
                 // Working with the Register Machine
                 //
                 Instruction::Move(value, target) => {
-                    self.registers.put(target, value.clone());
+                    match value {
+                        Value::Register(reg) => {
+                            self.registers.copy(&reg, &target);
+                        }
+                        v => {
+                            self.registers.put(&target, v.clone());
+                        }
+                    }
                     self.instr_ptr.next(&program);
                 }
 
@@ -73,7 +81,11 @@ impl Emulator {
                 }
 
                 Instruction::Clear(reg) => {
-                    self.registers.clear(reg);
+                    self.registers.clear(&reg);
+                    self.instr_ptr.next(&program);
+                }
+
+                Instruction::Deallocate { .. } | Instruction::Allocate { .. } => {
                     self.instr_ptr.next(&program);
                 }
 
@@ -100,17 +112,25 @@ impl Emulator {
                         },
                         &args,
                     );
-                    self.registers.put(destination, ret.into());
+                    self.registers.put(&destination, ret.into());
                     self.instr_ptr.next(&program);
                 }
 
+                Instruction::Call(FnCall::ApplyLambda { register }) => {
+                    if let Literal::Lambda(lambda) = self.registers.get(&register).into() {
+                        self.registers.fill_globals(&lambda.environment);
+                        self.instr_ptr.jump_to_label(&program, &lambda.first_label);
+                    } else {
+                        panic!(
+                            "Can not apply value found in {:?} since it is not a lambda",
+                            register
+                        )
+                    };
+                }
+
                 Instruction::Call(call) => {
-                    let arity = call.arity();
-                    let args = self.registers.get_many_literals_from_global_range(0, arity);
-                    let ret = runtime.execute(&call.into(), &args);
-                    self.registers.clear_globals(arity);
-                    self.registers.put_global(0, Value::Literal(ret));
-                    self.instr_ptr.next(&program);
+                    self.registers.clear_globals(call.arity());
+                    self.instr_ptr.call(&program, &call);
                 }
 
                 Instruction::TailCall(call) => {
@@ -127,7 +147,7 @@ impl Emulator {
                 // Flow-control operations
                 //
                 Instruction::Jump(label) => {
-                    trace!("Jump to label {:?}", label);
+                    trace!("Jump to label {}", label);
                     self.instr_ptr.jump_to_label(&program, &label);
                 }
 
@@ -140,7 +160,7 @@ impl Emulator {
                         trace!("Test passed! continuing...");
                         self.instr_ptr.next(&program);
                     } else {
-                        trace!("Test failed! Jumping to: {:?}", label);
+                        trace!("Test failed! Jumping to: {}", label);
                         self.instr_ptr.jump_to_label(&program, &label);
                     }
                 }
@@ -151,9 +171,18 @@ impl Emulator {
                 //
                 Instruction::ConsList { target, head, tail } => {
                     let head = Box::new(self.registers.get_literal_from_value(&head));
-                    let tail = Box::new(self.registers.get_literal_from_value(&tail));
+                    let tail = {
+                        let lit = self.registers.get_literal_from_value(&tail);
+                        match lit {
+                            Literal::List(t) => Box::new(t),
+                            _ => panic!(
+                                "Can not allow consing improper list with {} and {}",
+                                head, lit
+                            ),
+                        }
+                    };
                     self.registers
-                        .put(target, Literal::List(List::Cons(head, tail)).into());
+                        .put(&target, Literal::List(List::Cons(head, tail)).into());
                     self.instr_ptr.next(&program);
                 }
 
@@ -161,13 +190,11 @@ impl Emulator {
                     match self.registers.get(&list).into() {
                         Literal::List(List::Cons(boxed_head, boxed_tail)) => {
                             self.registers
-                                .put(head, Value::Literal(*boxed_head.clone()));
+                                .put(&head, Value::Literal(*boxed_head.clone()));
                             self.registers
-                                .put(tail, Value::Literal(*boxed_tail.clone()));
+                                .put(&tail, Literal::List(*boxed_tail.clone()).into());
                         }
-                        value => {
-                            panic!("Attempted to split a value that is not a list: {:?}", value)
-                        }
+                        value => panic!("Attempted to split a value that is not a list: {}", value),
                     };
                     self.instr_ptr.next(&program);
                 }
@@ -180,23 +207,54 @@ impl Emulator {
                     match self.registers.get(&tuple) {
                         Value::Literal(Literal::Tuple(Tuple { elements, .. })) => {
                             let element = elements[element as usize].clone();
-                            self.registers.put(target, Value::Literal(element));
+                            self.registers.put(&target, Value::Literal(element));
                         }
                         v => panic!(
-                            "Attempted to extract element from value that is not a tuple: {:?}",
+                            "Attempted to extract element from value that is not a tuple: {}",
                             v
                         ),
                     }
                     self.instr_ptr.next(&program);
                 }
 
-                Instruction::Kill => return Ok(EmulationStatus::Terminated),
+                Instruction::MakeLambda {
+                    module,
+                    first_label,
+                    arity,
+                } => {
+                    let environment = self.registers.get_many_literals_from_global_range(0, arity);
+                    self.registers.put(
+                        &Register::Global(0),
+                        Value::Literal(Literal::Lambda(Lambda {
+                            first_label,
+                            module,
+                            environment,
+                            arity,
+                        })),
+                    );
+                    self.instr_ptr.next(&program);
+                }
 
                 ////////////////////////////////////////////////////////////////
                 //
-                // Function calls
+                // Processes
                 //
-                _ => self.instr_ptr.next(&program),
+                Instruction::Kill => return Ok(EmulationStatus::Terminated),
+
+                Instruction::PidSelf(target) => {
+                    self.registers
+                        .put(&target, Literal::Pid(pid.clone()).into());
+                    self.instr_ptr.next(&program);
+                }
+
+                Instruction::Spawn
+                | Instruction::Monitor
+                | Instruction::Send
+                | Instruction::Receive => {
+                    self.instr_ptr.next(&program);
+                }
+
+                Instruction::Label(_) => self.instr_ptr.next(&program),
             }
             reductions += 1;
         }
@@ -215,16 +273,13 @@ impl Emulator {
             Test::IsNil(a) => match self.registers.get_literal_from_value(a) {
                 Literal::List(List::Nil) => true,
                 Literal::List(_) => false,
-                _ => panic!("Can not check if non list value {:?} is an empty list", a),
+                _ => panic!("Can not check if non list value {} is an empty list", a),
             },
 
             Test::IsNonEmptyList(a) => match self.registers.get_literal_from_value(a) {
                 Literal::List(List::Cons(_, _)) => true,
                 Literal::List(List::Nil) => false,
-                _ => panic!(
-                    "Can not check if non list value {:?} is a non empty list",
-                    a
-                ),
+                _ => panic!("Can not check if non list value {} is a non empty list", a),
             },
 
             Test::Equals(a, b) => {
@@ -240,7 +295,7 @@ impl Emulator {
                     Literal::Atom(tag) => tag == atom,
                     _ => false,
                 },
-                x => panic!("Cannot check if value {:?} is a tagged tuple", x),
+                x => panic!("Cannot check if value {} is a tagged tuple", x),
             },
         }
     }
