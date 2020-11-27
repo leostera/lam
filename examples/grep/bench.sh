bdf134d34c7816ba1d37fb9fe1a175180d0f1689
Leandro Ostera <leandro@ostera.io>
2020-11-27 10:37:16 +0100

    examples: grep, update sources and fix bug!

--- examples/grep/bench.sh
+++ examples/grep/bench.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-N=$*
-
-echo "Running grep ${N}..."
-
 erlc -S *.erl
 erlc *.erl
 
 lam build *.beam -o grep.opt.exe -t native -e grep
 lam build *.beam -o grep.opt.wasm -t wasm -e grep
 
-hyperfine \
-  --warmup 50 \
-  --ignore-failure \
-  "grep ${N}" \
-  "escript grep.erl ${N}" \
-  "wasmtime --dir=. ./grep.opt.wasm ${N}" \
-  "./grep.opt.exe ${N}"
+bench() {
+  local N=$*
+  echo "Running grep ${N}..."
+
+  hyperfine \
+    --warmup 50 \
+    --ignore-failure \
+    "grep ${N}" \
+    "escript grep.erl ${N}" \
+    "wasmtime --dir=. ./grep.opt.wasm ${N}" \
+    "./grep.opt.exe ${N}"
+}
+
+bench joe *.txt
--- examples/grep/grep.erl
+++ examples/grep/grep.erl
@@ -4,7 +4,7 @@
 -export([main/1]).
 
 print_matches(Word, [Word | T], File) ->
-  io:format(<<"~p: ~p\n">>, [File, Word]),
+  io:format(<<"~p | ~p: ~p\n">>, [self(), File, Word]),
   print_matches(Word, T, File);
 print_matches(Word, [_ | T], File)->
   print_matches(Word, T, File);
@@ -13,7 +13,8 @@ print_matches(_, _, _) -> ok.
 count(Top, File, Word) ->
   erlang:spawn(fun () ->
     {ok, Data} = file:read_file(File),
-    Words = binary:split(Data, <<" ">>, [global]),
+    Words = binary:split(Data, [<<" ">>, <<"\n">>], [global]),
+    io:format("~p ~p\n", [Word, Words]),
     print_matches(Word, Words, File),
     Top ! done
   end).
@@ -23,6 +24,8 @@ count_all([_|T]) -> receive done -> count_all(T) end.
 
 main([Word | Files]) ->
   Top = self(),
-  Run = fun (File) -> count(Top, File, Word) end,
+  Run = fun (File) -> count(Top, File, binary:list_to_bin(Word)) end,
+  io:format(<<"About to check ~p files\n">>, [length(Files)]),
   lists:foreach(Run, Files),
+  io:format(<<"Spawned ~p processes...awaiting replies.\n">>, [length(Files)]),
   count_all(Files).
