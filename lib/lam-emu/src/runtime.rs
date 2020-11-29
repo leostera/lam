57b4c02e3c120f42a51d75e812eaefe68e218b77
Leandro Ostera <leandro@ostera.io>
2020-11-29 13:48:11 +0100

    emu: split scheduler specific stuff from Runtime trait into its own
    This would allow us to reuse runtime bindings while swapped schedulers.

--- lib/lam-emu/src/runtime.rs
+++ lib/lam-emu/src/runtime.rs
@@ -1,13 +1,19 @@
+use super::coordinator::*;
 use super::literal::*;
 use super::program::*;
 use anyhow::Error;
 
+pub trait SchedulerManager {
+    fn setup(&mut self, scheduler_count: u32, program: &Program) -> Result<(), Error>;
+
+    fn run(&self, coordinator: &Coordinator) -> Result<(), Error>;
+}
+
 /// This trait represents a runtime system and is used to plug the emulator's side-effects and
 /// other functions implemented by the host.
+///
 pub trait Runtime {
     fn execute(&mut self, call: &MFA, args: &[Literal]) -> Literal;
-
-    fn run_schedulers(&mut self, scheduler_count: u32, program: &Program) -> Result<(), Error>;
 
     fn sleep(&self, duration: u64);
 
