d1818090deab56d611d17cd7e5f9fb0759791f76
Leandro Ostera <leandro@ostera.io>
2020-11-29 03:37:20 +0100

    emu: reformat docs

--- lib/lam-emu/src/runtime.rs
+++ lib/lam-emu/src/runtime.rs
@@ -2,9 +2,8 @@ use super::literal::*;
 use super::program::*;
 use anyhow::Error;
 
-/** This trait represents a runtime system and is used to plug the emulator's
- *  side-effects and other functions implemented by the host.
- */
+/// This trait represents a runtime system and is used to plug the emulator's side-effects and
+/// other functions implemented by the host.
 pub trait Runtime {
     fn execute(&mut self, call: &MFA, args: &[Literal]) -> Literal;
 
